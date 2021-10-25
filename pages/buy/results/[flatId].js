8039f4bcdfa1f06916e43f010ae58b1ed517d796
Wiola P <wiol@onet.eu>
2021-10-25 22:54:39 +0200

    add single offer route based on offer ID

--- pages/buy/results/[flatId].js
+++ pages/buy/results/[flatId].js
@@ -1,11 +1,46 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Head from 'next/head';
+import Offer from '../../../components/Offer';
+
+export async function getStaticPaths() {
+  const response = await fetch(`http://localhost:3000/api/offers`);
+  const data = await response.json();
+
+  const paths = data.map(offer => {
+    return {
+      params: {
+        flatId: `${offer.id}`
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(`http://localhost:3000/api/offers?id=${params.flatId}`);
+  const data = await response.json();
+  const offer = await data.filter(offer => offer.id === params.flatId && offer);
+
+  console.log(offer)
+
+  return {
+    props: {
+        offer: data,
+    },
+  }
+}
 
 
-function Flat() {
+function Flat({ offer }) {
   const router = useRouter();
   const flatId = router.query.flatId;
+  
 
   return (
     <>
@@ -18,6 +53,7 @@ function Flat() {
         <div>
           To jsou detaily nabidky cislo {flatId}
         </div>
+        {/* <Offer id={offer.id} region={offer.region} district={offer.district} size={offer.size} description={offer.description}/> */}
       </section>
     </>
   )
