d9825f7552e37922302af14d1250a8f52d45be01
Wiola P <wiol@onet.eu>
2021-10-25 22:53:52 +0200

    add region & district filters in Buy component

--- pages/buy/index.js
+++ pages/buy/index.js
@@ -1,9 +1,32 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+
+export async function getStaticProps() {
+    
+  const response = await fetch('http://localhost:3000/api/regions');
+  const data = await response.json();
+
+  return {
+    props: {
+        regions: data,
+    },
+    revalidate: 1,
+  }
+}
 
 
-function Buy() {
+function Buy({ regions }) {
 
+  const [regionsState, setRegionsState] = useState(Array.from(new Set(regions.map(region => region.region))));
+  const [districtsState, setDistrictsState] = useState();
+  const [chosenDistrict, setChosenDistrict] = useState('');
+
+  const setDistrictHandler = (e) => {
+    const chosenRegion = e.currentTarget.value;
+    setDistrictsState(regions.filter(region => region.region === chosenRegion && region.district));
+  };
+  
   
   return (
     <>
@@ -11,7 +34,7 @@ function Buy() {
         <title>BUY something!</title>
         <meta name="" content=""/>
       </Head>
-      <section class="buy">
+      <section className="buy">
         <header>
           <h1>Hledáte nemovitost ke koupi?</h1>
         </header>
@@ -20,24 +43,27 @@ function Buy() {
             <Link href="/">Zpet</Link>
             <form>
               <label>Typ nemovitosti:<br />
-                  <select type="select" name="estateType" >
-                      <option hidden disabled selected> ----- </option>
-                      <option value="byt">Byt</option>
-                      <option value="dum">Dům</option>
-                      <option value="pozemek">Pozemek</option>
-                  </select>
+                <select type="select" name="estateType" >
+                    <option hidden disabled selected> ----- </option>
+                    <option value="byt">Byt</option>
+                    <option value="dum">Dům</option>
+                    <option value="pozemek">Pozemek</option>
+                </select>
               </label>
               <label>Kraj ve kterém se nemovitost nachází:<br />
-                  <select type="select" name="region" >
-                      <option hidden disabled selected value="-----"> ----- </option> 
-                  </select>
+                <select type="select" name="region" onChange={(e) => setDistrictHandler(e)}>
+                    <option hidden disabled selected value="-----"> ----- </option> 
+                    {regionsState && regionsState.map(option => <option key={option} value={option}>{option}</option>)}
+                </select>
               </label>
               <label>Okres ve kterém se nemovitost nachází:<br />
-                  <select type="select" name="district">
-                      <option hidden disabled selected value="-----"> ----- </option>
-                  </select>
+                <select type="select" name="district" onChange={(e) => setChosenDistrict(e.currentTarget.value)}>
+                    <option hidden disabled selected value="-----"> ----- </option>
+                    {districtsState && districtsState.map(option => <option key={option.district} value={option.district}>{option.district}</option>)}
+                </select>
               </label>
-              <Link href="/buy/results">Ukaz nabidky</Link>
+              <Link href={`/buy/${chosenDistrict}`}>Ukaz nabidky podle okresu</Link>
+              <Link href={`/buy/results`}>Ukaz nabidky</Link>
           </form>
         </main>
       </section>
