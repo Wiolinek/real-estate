73157b13914cc6935fecf1ee81a0fed2bf503f59
Wiola P <wiol@onet.eu>
2021-10-30 22:16:06 +0200

    add Yup & getFieldProps

--- components/Form.js
+++ components/Form.js
@@ -1,5 +1,5 @@
 import { useFormik } from 'formik';
-
+import * as Yup from 'yup'; 
 
 
 function Form({ regionsState, districtsState, setDistrictHandler }) {
@@ -11,60 +11,69 @@ function Form({ regionsState, districtsState, setDistrictHandler }) {
         name: '',
         phone: '',
         email: '',
-    }
+    };
 
     const onSubmit = values => {
-        // console.log(values)
         console.log(values)
-    }
+    };
 
-    const validate = values => {
-        let errors = {};
+    // const validate = values => {
+    //     let errors = {};
 
-        if(!formik.values.estateType) {
-            formik.errors.estateType = 'This field is required'
-        } else {
-            formik.errors.estateType = ''
-        }
+    //     if(!formik.values.estateType) {
+    //         formik.errors.estateType = 'This field is required'
+    //     } else {
+    //         formik.errors.estateType = ''
+    //     }
 
-        if(formik.values.region === '') {
-            formik.errors.region = 'This field is required'
-        } else {
-            formik.errors.region = ''
-        }
+    //     if(formik.values.region === '') {
+    //         formik.errors.region = 'This field is required'
+    //     } else {
+    //         formik.errors.region = ''
+    //     }
 
-        if(formik.values.district === '') {
-            formik.errors.district = 'This field is required'
-        } else {
-            formik.errors.district = ''
-        }
+    //     if(formik.values.district === '') {
+    //         formik.errors.district = 'This field is required'
+    //     } else {
+    //         formik.errors.district = ''
+    //     }
 
-        if(formik.values.name === '') {
-            formik.errors.name = 'This field is required'
-        } else {
-            formik.errors.name = ''
-        }
+    //     if(formik.values.name === '') {
+    //         formik.errors.name = 'This field is required'
+    //     } else {
+    //         formik.errors.name = ''
+    //     }
 
-        if(formik.values.phone === '') {
-            formik.errors.phone = 'This field is required'
-        } else {
-            formik.errors.phone = ''
-        }
+    //     if(formik.values.phone === '') {
+    //         formik.errors.phone = 'This field is required'
+    //     } else {
+    //         formik.errors.phone = ''
+    //     }
 
-        if(formik.values.email === '') {
-            formik.errors.email = 'This field is required'
-        }
-        else if('regex') {
-            formik.errors.email = 'Invalid email format'
-        } else {
-            formik.errors.email = ''
-        }
-    }
+    //     if(formik.values.email === '') {
+    //         formik.errors.email = 'This field is required'
+    //     }
+    //     else if('regex') {
+    //         formik.errors.email = 'Invalid email format'
+    //     } else {
+    //         formik.errors.email = ''
+    //     }
+    // };
+
+    const validationSchema = Yup.object({
+        estateType: Yup.string().required('This field is required'),
+        region: Yup.string().required('This field is required'),
+        district: Yup.string().required('This field is required'),
+        name: Yup.string().required('This field is required'),
+        phone: Yup.string().required('This field is required'),
+        email: Yup.string().email('Invalid email format').required('This field is required'),
+    });
 
     const formik = useFormik({
         initialValues,
         onSubmit,
-        validate,
+        validationSchema,
+        // validate,
     });
 
     console.log(formik.touched)
@@ -73,7 +82,14 @@ function Form({ regionsState, districtsState, setDistrictHandler }) {
   return (
     <form onSubmit={formik.handleSubmit}>
         <label>Typ nemovitosti:<br />
-            <select type="select" name="estateType" value={formik.values.estateType} onChange={formik.handleChange} onBlur={formik.handleBlur}>
+            <select
+                type="select"
+                name="estateType"
+                {...formik.getFieldProps("estateType")}
+                // value={formik.values.estateType}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+            >
                 <option hidden disabled selected value={formik.values.estateType === '' ? formik.values.estateType : ' ----- '}> ----- </option>
                 <option value="byt">Byt</option>
                 <option value="dum">Dům</option>
@@ -82,29 +98,65 @@ function Form({ regionsState, districtsState, setDistrictHandler }) {
         </label>
         {formik.touched.estateType && formik.errors.estateType ? <p className="form-error">{formik.errors.estateType}</p> : null}
         <label>Kraj ve kterém se nemovitost nachází:<br />
-            <select type="select" name="region" value={formik.values.region} onChange={formik.handleChange} onBlur={formik.handleBlur} onMouseUp={e => setDistrictHandler(e)}>
+            <select
+                type="select"
+                name="region"
+                {...formik.getFieldProps("region")}
+                // value={formik.values.region}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                onMouseUp={e => setDistrictHandler(e)}
+            >
                 <option hidden disabled selected value={formik.values.region === '' ? formik.values.region : ' ----- '}> ----- </option> 
                 {regionsState && regionsState.map(option => <option key={option} value={option}>{option}</option>)}
             </select>
         </label>
         {formik.touched.region && formik.errors.region ? <p className="form-error">{formik.errors.region}</p> : null}
         <label>Okres ve kterém se nemovitost nachází:<br />
-            <select type="select" name="district" value={formik.values.district} onChange={formik.handleChange} onBlur={formik.handleBlur}>
+            <select
+                type="select"
+                name="district"
+                {...formik.getFieldProps("district")}
+                // value={formik.values.district}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+            >
                 <option hidden disabled selected value={formik.values.district === '' ? formik.values.district : ' ----- '}> ----- </option>
                 {districtsState && districtsState.map(option => <option key={option.district} value={option.district}>{option.district}</option>)}
             </select>
         </label>
         {formik.touched.district && formik.errors.district ? <p className="form-error">{formik.errors.district}</p> : null}
         <label>Jmeno:<br />
-            <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            <input
+                type="text"
+                name="name"
+                {...formik.getFieldProps("name")}
+                // value={formik.values.name}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+            />
         </label>
         {formik.touched.name && formik.errors.name ? <p className="form-error">{formik.errors.name}</p> : null}
         <label>Telefoní číslo:<br />
-            <input type="tel" name="phone" value={formik.values.phone} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            <input
+                type="tel"
+                name="phone"
+                {...formik.getFieldProps("phone")}
+                // value={formik.values.phone}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+            />
         </label>
         {formik.touched.phone && formik.errors.phone ? <p className="form-error">{formik.errors.phone}</p> : null}
         <label>Email:<br />
-            <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            <input
+                type="email"
+                name="email"
+                {...formik.getFieldProps("email")}
+                // value={formik.values.email}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+            />
         </label>
         {formik.touched.email && formik.errors.email ? <p className="form-error">{formik.errors.email}</p> : null}
         <button type="submit">Posli</button>
--- pages/sell.js
+++ pages/sell.js
@@ -1,13 +1,33 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Form from '../components/Form';
+import { useState } from 'react';
 
 
+export async function getStaticProps() {
+    
+  const response = await fetch('http://localhost:3000/api/regions');
+  const data = await response.json();
 
-function Sell() {
+  return {
+      props: {
+          regions: data,
+      },
+  }
+}
+
+function Sell({ regions }) {
+
+  const [regionsState, setRegionsState] = useState(Array.from(new Set(regions.map(region => region.region))));
+  const [districtsState, setDistrictsState] = useState();
+
+  const setDistrictHandler = e => {
+    setDistrictsState(regions.filter(region => region.region === e.target.value && region.district));
+};
+  
 
   return (
-    <>
+    <>  
       <Head>
         <title>SELL something!</title>
         <meta name="" content=""/>
@@ -17,10 +37,10 @@ function Sell() {
           <h1>Chcete prodát svoji nemovitost?</h1>
         </header>
         <main>
-          <p>Najdeme vám makléře, který všechno pro vás zaridi. Správně nafoti, naceni a zainzeruje. Stačí jen vyplnit nas krátký formulář o nemovitosti a hned vas budeme kontaktovat.
+          <p className="sale-text">Najdeme vám makléře, který všechno pro vás zaridi. Správně nafoti, naceni a zainzeruje. Stačí jen vyplnit nas krátký formulář o nemovitosti a hned vas budeme kontaktovat.
           </p>
           <Link href="/">Zpet</Link>
-          <Form />
+          <Form regionsState={regionsState} districtsState={districtsState} setDistrictHandler={setDistrictHandler} />
         </main>
       </section>
     </>
