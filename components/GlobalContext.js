cec6bbad5a9a0d7d3ff8fb054cfc5728d657b0a1
Wiola P <wiol@onet.eu>
2022-10-23 22:01:46 +0200

    add dotenv

--- components/GlobalContext.js
+++ components/GlobalContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { listHandler } from '/helpers/list.helper';
+require('dotenv').config();
 
 
 const GlobalContext = createContext({})
