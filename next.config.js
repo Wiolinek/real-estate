012266f9fde393a8191e5c9f8f4cdfbfa4717a84
Wiola P <wiol@onet.eu>
2022-10-26 00:30:50 +0200

    cors fix

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     CF_DELIVERY_ACCESS_TOKEN: process.env.CF_DELIVERY_ACCESS_TOKEN,
     CF_SPACE_ID: process.env.CF_SPACE_ID,
+    CORS_DESTINATION: process.env.CORS_DESTINATION
   },
   async headers() {
     return [
@@ -18,7 +19,7 @@ module.exports = {
         source: '/api/client',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'https://real-estate*' },
+          { key: 'Access-Control-Allow-Origin', value: process.env.CORS_DESTINATION },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
         ]
