e2624307cbe02ff3c3d7852024b643476da47df2
Wiola P <wiol@onet.eu>
2022-10-26 00:25:53 +0200

    cors fix

--- next.config.js
+++ next.config.js
@@ -11,13 +11,17 @@ module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     CF_DELIVERY_ACCESS_TOKEN: process.env.CF_DELIVERY_ACCESS_TOKEN,
     CF_SPACE_ID: process.env.CF_SPACE_ID,
-    CORS_DESTINATION: process.env.CORS_DESTINATION,
   },
-  async rewrites() {
+  async headers() {
     return [
       {
         source: '/api/client',
-        destination: 'https://real-estate-two-phi.vercel.app/api/client'
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://real-estate*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
+        ]
       }
     ]
   },
@@ -31,7 +35,7 @@ module.exports = {
     },
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ['@svgr/webpack']
     })
     return config
   }
